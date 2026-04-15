57ac6210117440516ab7b01945bdb89ac55754ef
srihitha <srihitha624@gmail.com>
2026-04-15 19:31:03 +0530

    allowed chages

--- medhahms/settings.py
+++ medhahms/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8e)wued+6q3=468vsa5pre6+8saxt(ldn-l)ql6yc$wg!(tk%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
